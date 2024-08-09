a0f30644fd4d51585e27499e76d915c514d736c6
Murad <agamedov94@mail.ru>
2024-08-09 19:17:48 +0400

    commit

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+function appendNumber(number) {
+  document.getElementById("display").value += number;
+}
+
+function appendOperator(operator) {
+  document.getElementById("display").value += operator;
+}
+
+function appendDot() {
+  document.getElementById("display").value += '.';
+}
+
+function clearDisplay() {
+  document.getElementById("display").value = '';
+}
+
+function calculate() {
+  let result = eval(document.getElementById("display").value);
+  document.getElementById("display").value = result;
+}
